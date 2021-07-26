21483807f6c79df52c452f9985cab030d62d457a
Sexettin <72075007+sexettin78@users.noreply.github.com>
2021-07-27 00:59:11 +0300

    Add files via upload

---
+++ sec/main.py
@@ -0,0 +1,16 @@
+import os
+print('''
+    _                ___       _.--.
+    \`.|\..----...-'`   `-._.-'_.-'`
+    /  ' `         ,       __.--'
+    )/' _/     \   `-_,   /	  1-)Sexettintool 2-)Anamenü
+    `-'" `"\_  ,_.-;_.-\_ ',     
+        _.-'_./   {_.'   ; /
+       {_.-``-'         {_/
+
+''')
+lnx = input("Hangisini Seçiyorsunuz > ")
+if(lnx=="1"):
+	os.system("python3 sec/sexettintool/sexettintoolsv10.py")
+elif(lnx=="2"):
+	os.system("python3 termkity.py")
