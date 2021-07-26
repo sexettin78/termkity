e6449b7672108ee4e61b7a3a48c68af6adb76059
Sexettin <72075007+sexettin78@users.noreply.github.com>
2021-07-27 01:02:11 +0300

    Add files via upload

---
+++ gelistirici/main.py
@@ -0,0 +1,14 @@
+import os
+print('''
+    _                ___       _.--.
+    \`.|\..----...-'`   `-._.-'_.-'`
+    /  ' `         ,       __.--'
+    )/' _/     \   `-_,   /	  1-)Anamenü
+    `-'" `"\_  ,_.-;_.-\_ ',     
+        _.-'_./   {_.'   ; /
+       {_.-``-'         {_/
+
+''')
+lnx = input("Hangisini Seçiyorsunuz > ")
+if(lnx=="1"):
+	os.system("python3 termkity.py")
